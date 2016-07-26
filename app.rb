48e16f0ed93bf53e1f9ed1b305f1d99e370e8f06
Roi Driscoll <roi.driscoll@gmail.com>
2016-07-26 15:40:48 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra/base'
+
+class LinkCollector < Sinatra::Base
+
+  get '/' do
+    "Working Hard"
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+ENV['RACK_ENV'] = "test"
+
+require_relative '../app.rb'
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = LinkCollector
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.order = :random
+  Kernel.srand config.seed
+end
