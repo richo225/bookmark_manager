def053a8c47dd6d98a02f22f2cfde45caa73fcd4
6eff <6eff@mail.ru>
2016-07-27 15:14:07 +0300

    adds a link to db

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'save website' do
+  # let(:link) { double Link.create(url: 'url_link', title: "new_link") }
+  scenario 'add title and address to BookmarkManager' do
+    visit '/'
+    click_link 'Add links'
+    fill_in('title', with: 'added_link')
+    fill_in('url', with: 'url_link')
+    click_button 'submit'
+
+    expect(current_path).to eq '/links'
+
+    within "ul#links" do
+      expect(page).to have_content 'added_link'
+    end
+  end
+end
